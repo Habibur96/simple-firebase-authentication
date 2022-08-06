a5c16003c485b0374124a16cfc17bddf3137ca88
Habibur96 <habiburrahmannayan66@gmail.com>
2022-08-06 06:33:11 +0600

    sign-out added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
-    // console.log('working')
+    // ====================================================
+    //                    Sign-In   
+    // =============================================
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch(error => {
         console.log('error', error);
       })
   }
+
+  // ====================================================
+  //                 Sign-Out
+  // =============================================
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+      .catch(error => {
+        setUser({})
+      })
+  }
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        //Using ternary operator
+        //Syntax: condition ? true : false
+        user.email ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      }
+
+      <h3>Name: {user.displayName}</h3>
+      <h3>Name: {user.displayName}</h3>
+      <h3>Email: {user.email}</h3>
+      <img src={user.photoURL} alt="" />
     </div>
+
   );
 }
 
