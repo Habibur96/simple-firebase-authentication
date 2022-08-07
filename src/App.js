d0b10b8e814b6b0c3a300a1b4bdfa99edcbc960f
Habibur96 <habiburrahmannayan66@gmail.com>
2022-08-08 01:57:44 +0600

    Sign In with github

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { useState } from 'react';
 const auth = getAuth(app);
 
 function App() {
   const [user, setUser] = useState({})
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+
+  const githubProvider = new GithubAuthProvider();
+
+
+  // ====================================================
+  //                    Sign-In   
+  // =============================================
   const handleGoogleSignIn = () => {
-    // ====================================================
-    //                    Sign-In   
-    // =============================================
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then(result => {
         const user = result.user;
         setUser(user);
@@ -20,6 +24,18 @@ function App() {
       })
       .catch(error => {
         console.log('error', error);
+      })
+  }
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch(error => {
+        console.error(error);
       })
   }
 
@@ -40,10 +56,16 @@ function App() {
       {
         //Using ternary operator
         //Syntax: condition ? true : false
-        user.email ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        user.email ? <button onClick={handleSignOut}>Sign Out</button> :
+          <>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
+
+            <button onClick={handleGithubSignIn}>Github Sign In</button>
+
+          </>
+
       }
 
-      <h3>Name: {user.displayName}</h3>
       <h3>Name: {user.displayName}</h3>
       <h3>Email: {user.email}</h3>
       <img src={user.photoURL} alt="" />
